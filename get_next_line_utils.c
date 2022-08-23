a5a3f1ca5151d3f39d71921d80c6155f0f65af4e
Joseph Kiragu <jkiragu@F3C027.42adel.org.au>
2022-08-23 19:07:22 +0930

    adding changes to utils.c

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -1,116 +1,106 @@
-# include "get_next_line.h"
+#include "get_next_line.h"
 
+char	*ft_strjoin(char *s1, char *s2)
+{
+	size_t	i;
+	size_t	j;
+	char	*str;
 
-size_t	ft_strlen(char *str)
+	i = 0;
+	j = 0;
+	if (s1 == NULL)
+	{
+		s1 = (char *)malloc(sizeof(char) * 1);
+		s1[0] = '\0';
+	}
+	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (str == NULL)
+		return (NULL);
+	while (s1[j] != '\0')
+		str[i++] = s1[j++];
+	j = 0;
+	while (s2[j] != '\0')
+		str[i++] = s2[j++];
+	str[i] = '\0';
+	free(s1);
+	return (str);
+}
+
+size_t	ft_strlen(char *s)
 {
 	size_t	i;
 
 	i = 0;
-	if (!str)
-		return (0);
-	while (*(str + i))
+	while (s[i] != 0)
 		i++;
 	return (i);
 }
 
-
-char	*ft_strchr(char *str, int c)
+char	*ft_strchr(char *src, int c)
 {
-	int	j;
+	size_t	i;
 
-	j = 0;
-	while (str[j])
+	i = 0;
+	if (src == NULL)
+		return (0);
+	if (c == '\0')
+		return ((char *)&src[ft_strlen(src)]);
+	while (src[i] != '\0')
 	{
-		if (str[j] == c)
-		{
-			return ((char *)str + j);
-		}
-		j++;
+		if (src[i] == (char)c)
+			return ((char *)&src[i]);
+		i++;
 	}
-	if (str[j] == c)
-	{
-		return ((char *)str + j);
-	}
-	return (NULL);
+	return (0);
 }
 
-char	*ft_strjoin(char *s1, char *s2)
+char	*ft_read_line(char *src)
 {
-	char	*str;
 	size_t	i;
-	size_t	j;
+	char	*str;
 
-	str = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
-	if (!str)
+	i = 0;
+	if (src == NULL)
+		return (NULL);
+	while (src[i] != '\0' && src[i] != '\n')
+		i++;
+	str = (char *)malloc(sizeof(char) * (i + 2));
+	if (str == NULL)
 		return (NULL);
 	i = 0;
-	j = 0;
-	while (s1[i])
+	while (src[i] != '\0' && src[i] != '\n')
 	{
-		str[j++] = s1[i];
+		str[i] = src[i];
 		i++;
 	}
-	i = 0;
-	while (s2[i])
-	{
-		str[j++] = s2[i];
-		i++;
-	}
-	str[j] = 0;
+	if (src[i] == '\n')
+		str[i++] = '\n';
+	str[i] = '\0';
 	return (str);
 }
 
-char    *ft_read_line (char *src)
+char	*ft_save(char *src)
 {
-    size_t i;
-    char *str;
+	size_t	i;
+	size_t	j;
+	char	*str;
 
-    i = 0;
-    if (src == NULL)
-        return (NULL);
-    while (src[i] != '\0' && src[i] != '\n')
-        i++;
-    str = malloc(sizeof(*str) * (i + 2)); // why 2 ?? maybe because of '\n' and '\0'
-    if (str == NULL)
-        return(NULL);
-    i = 0;
-
-    while (src[i] != '\0' && src[i] != '\n')
-    {
-        str[i] = src[i];
-        i++;
-    }
-    if (src[i] == '\n')
-        str[i++] = '\n';
-    str[i] = '\0';
-    return (str);
-}
-
-
-char    *ft_save(char *src)
-{
-    size_t  i;
-    size_t  j;
-    char    *str;
-
-    i = 0;
-    while (src[i] != '\0' && src[i] != '\n')
-        i++;
-    if (src[i] == '\0')
-    {
-        free(src);
-        return (0);
-    } // this statement returns if the line is the last one in the file
-    str = malloc(sizeof(*str) * (ft_strlen(src) - i + 1)); //str is everything apart from previous line
-    if (str == NULL)
-        return (NULL);
-    i = i + 1;
-    j = 0;
-    while (src[i] != '\0')
-        str[j++] = src[i++];
-    str[j] = '\0';
-    free(src); //src is freed here before being created in another loop.
-    return(str); 
-    
-
+	i = 0;
+	while (src[i] != '\0' && src[i] != '\n')
+		i++;
+	if (src[i] == '\0')
+	{
+		free(src);
+		return (0);
+	}
+	str = (char *)malloc(sizeof(char) * (ft_strlen(src) - i + 1));
+	if (str == NULL)
+		return (NULL);
+	i = i + 1;
+	j = 0;
+	while (src[i] != '\0')
+		str[j++] = src[i++];
+	str[j] = '\0';
+	free(src);
+	return (str);
 }
--- main.c
+++ main.c
@@ -1,21 +1,27 @@
-# include "get_next_line.h"
+#include "get_next_line.h"
 
 #include <stdio.h>
 
-int main(int argc, char **argv)
+int	main(int argc, char **argv)
 {
-    (void) argc;
-    (void) argv;
-    char *line;
-    
-    int fd = open("test.txt", O_RDONLY);
-    line = get_next_line(fd);
-    while (line != NULL)
-    {
-        printf("%s", line);
-        free(line); // freeing reseources for next line to be read
-        line = get_next_line(fd);
-    }
-    close(fd);
-    return (EXIT_SUCCESS);
+	(void)	argc;
+	(void)	argv;
+	char	*line;
+	char	*line1;
+	int	fd = open("test.txt", O_RDONLY);
+	line = get_next_line(fd);
+	int fd1 = open("test0.txt", O_RDONLY);
+	line1 = get_next_line(fd1);
+	while (line != NULL && line1 != NULL)
+	{
+		printf("%s", line);
+		printf("%s", line1);
+		free(line);
+		free(line1);
+		line = get_next_line(fd);
+		line1 = get_next_line(fd1);
+	}
+	close (fd);
+	close (fd1);
+	return (EXIT_SUCCESS);
 }
